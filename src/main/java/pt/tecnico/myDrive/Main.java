03a4e8b1fee6fb026d2cdd20d19297af6a40e21b
S-4-S <z4swlf@gmail.com>
2016-03-05 00:54:30 +0000

    Main name fix

--- src/main/java/pt/tecnico/myDrive/Main.java
+++ src/main/java/pt/tecnico/myDrive/Main.java
@@ -5,7 +5,7 @@ import java.io.PrintStream;
 
 import pt.ist.fenixframework.FenixFramework;
 
-public class MyDriveApplication {
+public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to MyDrive!");
