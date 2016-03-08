5ddfc66316c70cfe539c4b243dfc12bea88f17b1
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-03-08 17:02:55 +0000

    Add exception WrongSizePermissionException.java

---
+++ src/main/java/pt/tecnico/myDrive/exception/WrongSizePermissionException.java
@@ -0,0 +1,21 @@
+package pt.tecnico.myDrive.exception;
+
+public class WrongSizePermissionException extends MyDriveException {
+
+    private static final long serialVersionUID = 1L;
+
+    private int size;
+
+    public WrongSizePermissionException(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Expected different size got " + size;
+    }
+}
