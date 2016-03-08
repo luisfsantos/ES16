0f7fa69f34d5a352dfa67c3fdfe84993cbaf0c9f
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-03-08 11:54:44 +0000

    added Exceptions to setPermissions in User

--- src/main/java/pt/tecnico/myDrive/domain/User.java
+++ src/main/java/pt/tecnico/myDrive/domain/User.java
@@ -21,14 +21,14 @@ public class User extends User_Base {
 	}
 	*/
 	@Override
-	public void setUmask(String permission){
-		if(permission.length() != 8) return ;
+	public void setUmask(String permission) throws WrongSizeMaskStringException, InvalidCharMaskStringException{
+		if(permission.length() != 8) throw new WrongSizeMaskStringException(permisson.length());
 		Mask mask[] = Mask.values();
 		//String mask = "rwxdrwxd";
 		for(int i = 0; i < 8; i++)
 			//if(permission.charAt(i) != mask.charAt(i) && permission.charAt(i) != '-')
 			if(permission.charAt(i) != mask[4 % i].getValue() && permission.charAt(i) != '-')
-				return ;
+				throw new InvalidCharMaskStringException(permission.charAt(i), i);
 		super.setUmask(permission);
 	}
 
