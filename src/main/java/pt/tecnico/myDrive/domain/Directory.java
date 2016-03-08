efce043c2c01087db217b703551aa5538e66520f
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-03-08 14:20:45 +0000

    Added hasFile to Directory

--- src/main/java/pt/tecnico/myDrive/domain/Directory.java
+++ src/main/java/pt/tecnico/myDrive/domain/Directory.java
@@ -1,18 +1,27 @@
 package pt.tecnico.myDrive.domain;
 
 public class Directory extends Directory_Base {
-    
-    public Directory() {
-        super();
-    }
+	
+	public Directory() {
+		super();
+	}
 
-    public File getFile(String name) throws NoSuchFileInThisDirectoryException{
-    	Iterator iterator = getFileSet().iterator();
-    	while(iterator.hasNext()){
-    		File file = iterator.next();
-    		if(file.getName() == name) return file;
-    	}
-    	throw new NoSuchFileInThisDirectoryException(name);
-    }
-    
+	public File getFile(String name) throws NoSuchFileInThisDirectoryException{
+		Iterator iterator = getFileSet().iterator();
+		while(iterator.hasNext()){
+			File file = iterator.next();
+			if(file.getName() == name) return file;
+		}
+		throw new NoSuchFileInThisDirectoryException(name);
+	}
+
+	public boolean hasFile(String name){
+		Iterator iterator = getFileSet().iterator();
+		while(iterator.hasNext()){
+			File file = iterator.next();
+			if(file.getName() == name) return true;
+		}
+		return false;
+	}
+	
 }
