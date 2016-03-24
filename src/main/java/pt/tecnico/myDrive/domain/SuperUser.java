52dc833885224a3acdc62ae9afe5803e9cd06b47
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-03-24 15:25:26 +0000

    Fix wrong merge with d5fd24a

--- src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
+++ src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
@@ -1,14 +1,25 @@
 package pt.tecnico.myDrive.domain;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pt.tecnico.myDrive.exception.CannotSetRootUsernameException;
+
 public class SuperUser extends SuperUser_Base {
-    
+
+	static final Logger log = LogManager.getRootLogger();
+
     public SuperUser(Manager manager) {
-		this.setUsername("root");
     	this.setManager(manager);
+		this.setRootUsername("root");
 		this.setPassword("***");
 		this.setName("Super User");
 		this.setUmask("rwxdr-x-");
     }
+
+    @Override
+	public void setUsername(String username){
+		throw new CannotSetRootUsernameException();
+	}
 
 	public boolean hasPermission(File file, Mask mask){
 		return true;
--- src/main/java/pt/tecnico/myDrive/domain/User.java
+++ src/main/java/pt/tecnico/myDrive/domain/User.java
@@ -61,6 +61,11 @@ public class User extends User_Base {
 		this.setName(name);
 	}
 
+	protected void setRootUsername(String username){
+		super.setUsername(username);
+	}
+
+
 	@Override
 	public void setUsername(String username) throws UserAlreadyExistsException, EmptyUsernameException, InvalidUsernameException{
 		
