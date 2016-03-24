d2b7e2e3b00bd03a82080826dba9491144ac3aa5
Frederico Teixeira <frederico.teixeira@tecnico.ulisboa.pt>
2016-03-24 06:42:36 +0000

    Fix setManager is use before setUsername

--- src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
+++ src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
@@ -3,8 +3,8 @@ package pt.tecnico.myDrive.domain;
 public class SuperUser extends SuperUser_Base {
     
     public SuperUser(Manager manager) {
-    	this.setManager(manager);
 		this.setUsername("root");
+    	this.setManager(manager);
 		this.setPassword("***");
 		this.setName("Super User");
 		this.setUmask("rwxdr-x-");
