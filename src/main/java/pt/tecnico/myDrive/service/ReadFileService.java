9dd873112f7633a308e77e3671158383ea069d5d
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-04 13:37:30 +0100

    add ReadFileService stub to facilitate Test creation

---
+++ src/main/java/pt/tecnico/myDrive/service/ReadFileService.java
@@ -0,0 +1,21 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class ReadFileService extends MyDriveService {
+
+	public ReadFileService() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ReadFileService(long token, String name) {
+		// TODO 
+	}
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
