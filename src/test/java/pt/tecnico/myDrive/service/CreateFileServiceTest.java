1814a64847d0d920cdad6054640b7e0acbb582a5
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-04 13:27:40 +0100

    add file creation tests and noSession Added tests 2 to 6 to test that files can be created and that when no file is specified an exception is thrown. Added a test to verify that if a token has no session then it fails.

--- src/test/java/pt/tecnico/myDrive/service/CreateFileServiceTest.java
+++ src/test/java/pt/tecnico/myDrive/service/CreateFileServiceTest.java
@@ -2,16 +2,27 @@ package pt.tecnico.myDrive.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigInteger;
+import java.util.Random;
 
 import org.junit.Test;
 
 import pt.tecnico.myDrive.domain.Manager;
 import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.PlainFile;
+import pt.tecnico.myDrive.domain.Link;
+import pt.tecnico.myDrive.domain.App;
 import pt.tecnico.myDrive.domain.Login;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class CreateFileServiceTest extends AbstractServiceTest {
 	private Long token;
 	private Directory home;
+	private String name = "newFile";
 	@Override
 	protected void populate() {
 		Manager m = Manager.getInstance();
@@ -25,10 +36,46 @@ public class CreateFileServiceTest extends AbstractServiceTest {
 	
 	@Test
 	public void successNewDirectory() {
-		final String directoryName = "newDirectory";
-		CreateFileService service = new CreateFileService(token, directoryName, "dir", null);
+		CreateFileService service = new CreateFileService(token, name, "dir", null);
 		service.execute();
-		assertEquals("File not created" , true, home.hasFile(directoryName));
+		assertEquals("Directory not created" , true, home.hasFile(name));
+		assertThat(home.getFileByName(name), instanceOf(Directory.class));
+	}
+	
+	@Test
+	public void successNewPlainFile() {
+		CreateFileService service = new CreateFileService(token, name, "plain", null);
+		service.execute();
+		assertEquals("Plain File not created" , true, home.hasFile(name));
+		assertThat(home.getFileByName(name), instanceOf(PlainFile.class));
+	}
+	
+	@Test
+	public void successNewApp() {
+		CreateFileService service = new CreateFileService(token, name, "app", null);
+		service.execute();
+		assertEquals("App not created" , true, home.hasFile(name));
+		assertThat(home.getFileByName(name), instanceOf(App.class));
+	}
+	
+	@Test
+	public void successNewLink() {
+		CreateFileService service = new CreateFileService(token, name, "link", null);
+		service.execute();
+		assertEquals("Link not created" , true, home.hasFile(name));
+		assertThat(home.getFileByName(name), instanceOf(Link.class));
 	}
 
+	@Test(expected = MyDriveException.class) //TODO will depend on actual exception for wrong file type
+	public void noFileType() {
+		CreateFileService service = new CreateFileService(token, name, null, null);
+		service.execute();
+	}
+	
+	@Test(expected = MyDriveException.class) //TODO will depend on actual exception for wrong file type
+	public void noSession() {
+		final long noSessionToken = new BigInteger(64, new Random()).longValue();
+		CreateFileService service = new CreateFileService(noSessionToken, name, "dir", null);
+		service.execute();
+	}
 }
