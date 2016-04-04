305896dc436746a326a159acf06d21a798c023ac
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-04 01:35:26 +0100

    fix path for tests

---
+++ src/test/java/pt/tecnico/myDrive/service/AbstractServiceTest.java
@@ -0,0 +1,47 @@
+package pt.tecnico.myDrive.service;
+
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.core.WriteOnReadError;
+
+public abstract class AbstractServiceTest {
+
+	protected static final Logger log = LogManager.getRootLogger();
+
+    @BeforeClass // run once before each test class
+    public static void setUpBeforeAll() throws Exception {
+	// run tests with a clean database!!!
+	
+    }
+
+    @Before // run before each test
+    public void setUp() throws Exception {
+        try {
+            FenixFramework.getTransactionManager().begin(false);
+            populate();
+        } catch (WriteOnReadError | NotSupportedException | SystemException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @After // rollback after each test
+    public void tearDown() {
+        try {
+            FenixFramework.getTransactionManager().rollback();
+        } catch (IllegalStateException | SecurityException | SystemException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected abstract void populate(); // each test adds its own data
+
+}
