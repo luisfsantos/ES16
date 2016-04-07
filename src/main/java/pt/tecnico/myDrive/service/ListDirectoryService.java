ba22ad1a97f27e5da1c2927adecfa8a209c68909
Miguel Carvalho <migueloak59@hotmail.com>
2016-04-07 18:49:40 +0100

    add skeleton for ListDirectoryService

---
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
@@ -0,0 +1,21 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class ListDirectoryService extends MyDriveService{
+
+
+    public ListDirectoryService(){
+        //TODO
+    }
+
+    public ListDirectoryService(long token){
+
+    }
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+        // TODO Auto-generated method stub
+
+    }
+}
