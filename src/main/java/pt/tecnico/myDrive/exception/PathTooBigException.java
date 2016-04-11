12d3f7eb5a46734d993413f8488836b253149d3a
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-04-11 16:27:21 +0100

    Create PathTooBigException

---
+++ src/main/java/pt/tecnico/myDrive/exception/PathTooBigException.java
@@ -0,0 +1,13 @@
+package pt.tecnico.myDrive.exception;
+
+public class PathTooBigException extends MyDriveException {
+
+    private static final long serialVersionUID = 1L;
+
+    public PathTooBigException(String path) {}
+
+    @Override
+    public String getMessage() {
+        return "Path is too big";
+    }
+}
