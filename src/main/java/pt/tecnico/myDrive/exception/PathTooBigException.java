ace30a183cc49e73f7f0b91983dd4ba4e45ac291
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-04-11 16:28:51 +0100

    Remove args from PathTooBigException

--- src/main/java/pt/tecnico/myDrive/exception/PathTooBigException.java
+++ src/main/java/pt/tecnico/myDrive/exception/PathTooBigException.java
@@ -4,7 +4,7 @@ public class PathTooBigException extends MyDriveException {
 
     private static final long serialVersionUID = 1L;
 
-    public PathTooBigException(String path) {}
+    public PathTooBigException() {}
 
     @Override
     public String getMessage() {
