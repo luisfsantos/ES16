bdbfc8dc6991173e8e953140314575ecdbd06da5
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-11 18:30:50 +0100

    add ChangeDirectoryService and Test

---
+++ src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
@@ -0,0 +1,22 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class ChangeDirectoryService extends TokenValidationService {
+
+	public ChangeDirectoryService(Long token, String path) {
+		super(token);
+		// FIXME
+	}
+	
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// FIXME
+	}
+
+	public String result() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
