b2280a2ec773477e703426b0410a70c8ac6e4abf
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-04-12 19:12:56 +0100

    Create LoginService Class

---
+++ src/main/java/pt/tecnico/myDrive/service/LoginService.java
@@ -0,0 +1,18 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class LoginService extends MyDriveService {
+
+    private String name;
+    private String password;
+
+    public LoginService(String name, String password) {}
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
