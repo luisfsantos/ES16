124ef6079a9f15f1603370be23c23e96ccec7640
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-13 18:29:27 +0100

    "fixes #68; critical fix taken from read file service"

--- src/main/java/pt/tecnico/myDrive/domain/File.java
+++ src/main/java/pt/tecnico/myDrive/domain/File.java
@@ -154,7 +154,7 @@ public abstract class File extends File_Base {
 	
 	public abstract File lookup(String path, User user);
 
-	public abstract File lookup(String path, User user, int psize);
+	abstract File lookup(String path, User user, int psize);
 
 }
 
