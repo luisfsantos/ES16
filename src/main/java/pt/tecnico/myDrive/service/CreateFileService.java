1f705475524e87bd6c03371708bfe193a1fc12f0
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-04-13 13:57:34 +0100

    CreateFileService

--- src/main/java/pt/tecnico/myDrive/service/CreateFileService.java
+++ src/main/java/pt/tecnico/myDrive/service/CreateFileService.java
@@ -1,5 +1,7 @@
 package pt.tecnico.myDrive.service;
 
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.myDrive.domain.*;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class CreateFileService extends TokenValidationService {
@@ -11,11 +13,38 @@ public class CreateFileService extends TokenValidationService {
 	
 	public CreateFileService(Long token, String name, String type, String contents) {
 		super(token);
+		this.name = name;
+		this.type = type;
+		this.contents = contents;
 	}
 
 	@Override
 	protected void dispatch() throws MyDriveException {
-		// TODO Auto-generated method stub
+		Manager manager = Manager.getInstance();
+		Login login = manager.getLoginByToken(token);
+		User user = login.getCurrentUser();
+		Directory parentDir = login.getCurrentDir();
+
+		if (login.validateToken(token)){
+			switch (type) {
+				case "app":
+					new App(name, user, parentDir, contents);
+					break;
+				case "dir":
+					//FIXME directory must not have contents
+					new Directory(name, user, parentDir);
+					break;
+				case "link":
+					new Link(name, user, parentDir, contents);
+					break;
+				case "plain":
+					new PlainFile(name, user, parentDir, contents);
+					break;
+				default:
+					//FIXME type must be one of the above
+					break;
+			}
+		}
 
 	}
 
