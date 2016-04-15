ec0141944af86a4fea73612a07a5239aa09c61d9
S-4-S <z4swlf@gmail.com>
2016-04-15 11:10:59 +0100

    set TokenValidationServiceTest as abstract

--- src/main/java/pt/tecnico/myDrive/domain/Directory.java
+++ src/main/java/pt/tecnico/myDrive/domain/Directory.java
@@ -3,7 +3,12 @@ package pt.tecnico.myDrive.domain;
 import org.jdom2.Element;
 import org.joda.time.DateTime;
 
-import pt.tecnico.myDrive.exception.*;
+import pt.tecnico.myDrive.exception.AccessDeniedException;
+import pt.tecnico.myDrive.exception.AccessDeniedToManipulateLoginException;
+import pt.tecnico.myDrive.exception.CannotReadException;
+import pt.tecnico.myDrive.exception.FileDoesntExistsInDirectoryException;
+import pt.tecnico.myDrive.exception.IsHomeDirectoryException;
+import pt.tecnico.myDrive.exception.PathTooBigException;
 
 import java.io.UnsupportedEncodingException;
 import java.util.*;
