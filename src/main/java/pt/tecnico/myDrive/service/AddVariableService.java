5c26125219d46977a6ef58ce9435c37f879380b9
S-4-S <z4swlf@gmail.com>
2016-04-22 22:12:08 +0100

    add service and test classes to AddVariableService

---
+++ src/main/java/pt/tecnico/myDrive/service/AddVariableService.java
@@ -0,0 +1,20 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class AddVariableService extends MyDriveService {
+	
+	private Long token;
+ 	private String varName;
+ 	private String value;
+ 	
+ 	public AddVariableService(Long token, String varName, String value) {
+ 		// TODO
+ 	}
+ 	
+ 	
+ 	@Override 
+ 	protected void dispatch() throws MyDriveException {
+ 		
+ 	}
+ }
---
+++ src/test/java/pt/tecnico/myDrive/service/AddVariableServiceTest.java
@@ -0,0 +1,17 @@
+package pt.tecnico.myDrive.service;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AddVariableServiceTest  extends TokenValidationServiceTest {
+
+	
+	@Override
+	protected void populate() {
+		super.populate();
+
+	}
+	
+
+
+}
