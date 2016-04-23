1403a7a2d582d2156140773a61bed359175f9f53
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-04-23 14:47:04 +0100

    add ability to keep the active token and a list logged in users

--- src/main/java/pt/tecnico/myDrive/presentation/MyDrive.java
+++ src/main/java/pt/tecnico/myDrive/presentation/MyDrive.java
@@ -1,17 +1,28 @@
 package pt.tecnico.myDrive.presentation;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class MyDrive extends Shell {
+	protected Map<String, Long> loggedIn = new HashMap<>(); //TODO must users have more than one active session?
 
 	public static void main(String[] args) throws Exception {
 		MyDrive sh = new MyDrive();
 	    sh.execute();
 	  }
 
-	  public MyDrive() { // add commands here
-		  super("MyDrive");
-		  /* eg:
-		  new CreateFile(this); // the CreateFile command class has to exist and extend from myDriveCommand
-		   */
-	  }
+	public MyDrive() { // add commands here
+	  super("MyDrive");
+	  /* eg:
+	  new CreateFile(this); // the CreateFile command class has to exist and extend from MyDriveCommand
+	   */
+	}
 
+	public void addLogin(String username, Long newToken) {
+		loggedIn.put(username, newToken);
+	}
+
+	public void getUserToken(String username) {
+		loggedIn.get(username);
+	}
 }
