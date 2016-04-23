6f70645f996e28ffd83267a494235debc096e4bc
S-4-S <z4swlf@gmail.com>
2016-04-23 18:37:08 +0100

    populate and first test to AddVariableServiceTest

--- src/test/java/pt/tecnico/myDrive/service/AddVariableServiceTest.java
+++ src/test/java/pt/tecnico/myDrive/service/AddVariableServiceTest.java
@@ -3,15 +3,29 @@ package pt.tecnico.myDrive.service;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class AddVariableServiceTest  extends TokenValidationServiceTest {
+import pt.tecnico.myDrive.domain.EnvironmentVariable;
+import pt.tecnico.myDrive.domain.Login;
 
+public class AddVariableServiceTest  extends TokenValidationServiceTest {
+	Long token;
+	String existantName = "ExistantName";
+	String existantValue = "ExistantValue";
+	String validString = "ValidString";
 	
 	@Override
 	protected void populate() {
 		super.populate();
-
+		Login rootLogin = new Login("root", "***");
+		token = rootLogin.getToken();
+		
+		new EnvironmentVariable (rootLogin, existantName, existantValue);
 	}
 	
 
-
+	@Test(expected = InvalidEnvironmentVarNameException.class)
+	public void nullName() {
+		AddVariableService service = new AddVariableService(token, null, validString);
+		service.execute();
+	}
+	
 }
