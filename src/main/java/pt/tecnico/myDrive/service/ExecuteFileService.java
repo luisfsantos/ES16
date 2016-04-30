f9849aedfe46cb3da7b5c2ae112a1a5a713adf19
S-4-S <z4swlf@gmail.com>
2016-04-30 14:42:10 +0100

    implement ExecuteFileService

--- src/main/java/pt/tecnico/myDrive/service/ExecuteFileService.java
+++ src/main/java/pt/tecnico/myDrive/service/ExecuteFileService.java
@@ -1,15 +1,27 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.File;
+import pt.tecnico.myDrive.exception.InvalidPathException;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ExecuteFileService extends TokenValidationService {
 
+	private String path;
+	private String[] args;
+	
     public ExecuteFileService(long token, String path, String[] args) {
         super(token);
+        this.path = path;
+        this.args = args;
     }
 
     @Override
     protected void dispatch() throws MyDriveException {
-        //TODO
+    	super.dispatch();
+    	File execFile = session.getCurrentDir().lookup(path, session.getCurrentUser());
+    	if (execFile.equals(null)) {
+    		throw new InvalidPathException(path);
+    	}
+    	execFile.execute(session.getCurrentUser(), args);
     }
 }
