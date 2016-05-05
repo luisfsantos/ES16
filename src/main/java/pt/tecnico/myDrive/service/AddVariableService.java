12a4433077ceace3627bbf972c98741580afaa16
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-05-05 11:23:21 +0100

    Implement AddVariableService constructor

--- src/main/java/pt/tecnico/myDrive/service/AddVariableService.java
+++ src/main/java/pt/tecnico/myDrive/service/AddVariableService.java
@@ -1,8 +1,11 @@
 package pt.tecnico.myDrive.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
+import pt.tecnico.myDrive.domain.EnvironmentVariable;
 import pt.tecnico.myDrive.exception.MyDriveException;
 import pt.tecnico.myDrive.service.dto.VariableDto;
 
@@ -14,12 +17,26 @@ public class AddVariableService extends TokenValidationService {
  	private List<VariableDto> variables = new ArrayList<>();
  	
  	public AddVariableService(Long token, String varName, String value) {
- 		super(token);
+		super(token);
+		this.varName = varName;
+		this.value = value;
  	}
  	
  	
  	@Override 
  	protected void dispatch() throws MyDriveException {
+		super.dispatch();
+
+		Set<EnvironmentVariable> environmentVariables = session.getEnvironmentVariableSet();
+
+		for (EnvironmentVariable i : environmentVariables) {
+			if(i.getName().equals(varName)) {
+				i.setValue(value);
+			}
+			VariableDto newDto = new VariableDto(i.getName(), i.getValue());
+			variables.add(newDto);
+
+		}
 
  	}
  	
