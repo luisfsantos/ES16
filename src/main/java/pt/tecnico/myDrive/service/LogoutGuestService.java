7f4e03e9934b828a9f55b9b54e253b648448e8e3
dsilva44 <d.silva44@hotmail.com>
2016-05-08 21:18:21 +0100

    add LogoutGuestService structure and implementation of tests

---
+++ src/main/java/pt/tecnico/myDrive/service/LogoutGuestService.java
@@ -0,0 +1,15 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class LogoutGuestService extends MyDriveService{
+
+
+    public LogoutGuestService(Long token){
+    }
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+
+    }
+}
---
+++ src/test/java/pt/tecnico/myDrive/service/LogoutGuestServiceTest.java
@@ -0,0 +1,43 @@
+package pt.tecnico.myDrive.service;
+
+import org.junit.Test;
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.Manager;
+import static org.junit.Assert.assertNull;
+
+public class LogoutGuestServiceTest extends AbstractServiceTest{
+
+    private Manager manager;
+    private Long guestToken;
+    private Long rootToken;
+
+    @Override
+    protected void populate() {
+        manager = Manager.getInstance();
+
+        Login guestLogin = new Login("nobody", "");
+        guestToken = guestLogin.getToken();
+
+        Login rootLogin = new Login("root", "***");
+        rootToken = rootLogin.getToken();
+    }
+
+    @Test
+    public void verifyLogoutGuestCorrectToken(){
+        LogoutGuestService service = new LogoutGuestService(guestToken);
+        service.execute();
+
+        Login log = manager.getLoginByToken(guestToken);
+
+        assertNull("guest login is not null", log);
+    }
+
+/*
+    @Test(expected = InvalidGuestToken.class)
+    public void verifyLogoutGuestIncorrectToken(){
+        LogoutGuestService service = new LogoutGuestService(rootToken);
+        service.execute();
+    }
+*/
+
+}
