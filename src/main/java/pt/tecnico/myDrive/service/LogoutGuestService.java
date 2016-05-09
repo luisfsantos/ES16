d87ac398e01b7981d86c3b74e7e1193861f1ca44
S-4-S <z4swlf@gmail.com>
2016-05-09 12:50:00 +0100

    code logout guest service

--- src/main/java/pt/tecnico/myDrive/service/LogoutGuestService.java
+++ src/main/java/pt/tecnico/myDrive/service/LogoutGuestService.java
@@ -1,15 +1,24 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.Manager;
+import pt.tecnico.myDrive.exception.InvalidTokenException;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class LogoutGuestService extends MyDriveService{
-
+	Long token;
 
     public LogoutGuestService(Long token){
+    	this.token = token;
     }
 
     @Override
     protected void dispatch() throws MyDriveException {
-
+    	Manager m = getManager();
+    	Login login = m.getLoginByToken(token);
+    	if (login == null) {
+    		throw new InvalidTokenException();
+    	}
+    	login.remove();
     }
 }
