73de795e2b9d223a605e708c2eb9dacee78d322f
pedrofffernandes <pedro.f.fernandes@tecnico.ulisboa.pt>
2016-05-12 11:41:43 +0100

    Fix ImportServiceTest

--- src/test/java/pt/tecnico/myDrive/service/ImportServiceTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ImportServiceTest.java
@@ -13,40 +13,47 @@ import java.io.StringReader;
 public class ImportServiceTest extends AbstractServiceTest{
 
 	private final String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-	+"<myDrive>"
-	+"<user username=\"jtb\">"
-	+"  <password>fermento</password>"
-	+"  <name>Joaquim Teófilo Braga</name>"
-	+"  <home>/home/jtb</home>"
-	+"  <mask>rwxdr-x-</mask>"
-	+"</user>"
-	+"<plain id=\"3\">"
-	+"  <path>/home/jtb</path>"
-	+"  <name>profile</name>"
-	+"  <owner>jtb</owner>"
-	+"  <perm>rw-dr---</perm>"
-	+"  <contents>Primeiro chefe de Estado do regime republicano (acumulando com a chefia do governo), numa capacidade provisória até à eleição do primeiro presidente da República.</contents>"
-	+"</plain>"
-	+"<dir id=\"4\">"
-	+"  <path>/home/jtb</path>"
-	+"  <name>documents</name>"
-	+"  <owner>jtb</owner>"
-	+"  <perm>rwxdr-x-</perm>"
-	+"</dir>"
-	+"<link id=\"5\">"
-	+"  <path>/home/jtb</path>"
-	+"  <name>doc</name>"
-	+"  <owner>jtb</owner>"
-	+"  <perm>r-xdr-x-</perm>"
-	+"  <value>/home/jtb/documents</value>"
-	+"</link>"
-	+"<dir id=\"7\">"
-	+"  <path>/home/jtb</path>"
-	+"  <owner>jtb</owner>"
-	+"  <name>bin</name>"
-	+"  <perm>rwxd--x-</perm>"
-	+"</dir>"
-	+"</myDrive>";
+			+"<myDrive>"
+			+"<user username=\"jtb\">"
+			+"  <password>fermento</password>"
+			+"  <name>Joaquim Teófilo Braga</name>"
+			+"  <home>/home/jtb</home>"
+			+"  <mask>rwxdr-x-</mask>"
+			+"</user>"
+			+"<plain id=\"3\">"
+			+"  <path>/home/jtb</path>"
+			+"  <name>profile</name>"
+			+"  <owner>jtb</owner>"
+			+"  <perm>rw-dr---</perm>"
+			+"  <contents>Primeiro chefe de Estado do regime republicano (acumulando com a chefia do governo), numa capacidade provisória até à eleição do primeiro presidente da República.</contents>"
+			+"</plain>"
+			+"<dir id=\"4\">"
+			+"  <path>/home/jtb</path>"
+			+"  <name>documents</name>"
+			+"  <owner>jtb</owner>"
+			+"  <perm>rwxdrwxd</perm>"
+			+"</dir>"
+			+"<link id=\"5\">"
+			+"  <path>/home/jtb</path>"
+			+"  <name>doc</name>"
+			+"  <owner>jtb</owner>"
+			+"  <perm>r-xdr-x-</perm>"
+			+"  <value>/home/jtb/documents</value>"
+			+"</link>"
+			+"<plain id=\"8\">"
+			+"  <path>/home/jtb/documents</path>"
+			+"  <name>file</name>"
+			+"  <owner>jtb</owner>"
+			+"  <perm>rwxdr-x-</perm>"
+			+"  <contents>Pri</contents>"
+			+"</plain>"
+			+"<dir id=\"7\">"
+			+"  <path>/home/jtb</path>"
+			+"  <owner>jtb</owner>"
+			+"  <name>bin</name>"
+			+"  <perm>rwxd--x-</perm>"
+			+"</dir>"
+			+"</myDrive>";
 
 
 	@Override
@@ -62,11 +69,11 @@ public class ImportServiceTest extends AbstractServiceTest{
 
 		Manager manager = Manager.getInstance();
 
-		assertTrue(manager.hasUser("jtb"));
-		assertEquals(manager.fetchUser("jtb", "fermento").getHome().getFileByName("profile").read(manager.fetchUser("jtb", "fermento")), "Primeiro chefe de Estado do regime republicano (acumulando com a chefia do governo), numa capacidade provisória até à eleição do primeiro presidente da República.");
-		assertEquals(manager.fetchUser("jtb", "fermento").getHome().getFileByName("documents").getPermissions(), "rwxdr-x-");
-		assertEquals(manager.fetchUser("jtb", "fermento").getHome().getFileByName("doc").read(manager.fetchUser("jtb", "fermento")), "/home/jtb/documents");
-		assertEquals(manager.fetchUser("jtb", "fermento").getHome().getFileByName("bin").getPermissions(), "rwxd--x-");
+		assertTrue("1", manager.hasUser("jtb"));
+		assertEquals("2", manager.fetchUser("jtb", "fermento").getHome().getFileByName("profile").read(manager.fetchUser("jtb", "fermento")), "Primeiro chefe de Estado do regime republicano (acumulando com a chefia do governo), numa capacidade provisória até à eleição do primeiro presidente da República.");
+		assertEquals("3", "rwxdrwxd", manager.fetchUser("jtb", "fermento").getHome().getFileByName("documents").getPermissions());
+		assertEquals("4", "file", manager.fetchUser("jtb", "fermento").getHome().lookup("/home/jtb/doc/file", manager.fetchUser("jtb", "fermento")).getName());
+		assertEquals("5", "rwxd--x-", manager.fetchUser("jtb", "fermento").getHome().getFileByName("bin").getPermissions());
 	}
 
 
